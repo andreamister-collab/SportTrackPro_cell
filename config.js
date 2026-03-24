437252bb1fcfc0fea49883ed9b59dc9d2128cceb
andreamister-collab <andrea.mister@gmail.com>
2026-03-24 14:35:26 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,2 @@
+export const supabaseUrl = "https://jvcplfgozdqirzqfqwox.supabase.co";
+export const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2Y3BsZmdvemRxaXJ6cWZxd294Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1OTIxMjIsImV4cCI6MjA4OTE2ODEyMn0.-W724h8dTtaAyV1HxydYRddoD1oRxmEm0-zZc_8bxx8";
