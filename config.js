1ad071c71fba0fca9a1c98743f8704b68cf44fd5
andreamister-collab <andrea.mister@gmail.com>
2026-03-26 21:50:07 +0100

    Delete config.js

--- config.js
+++
@@ -1,2 +0,0 @@
-const SUPABASE_URL  = 'https://saoghfehqrjgobcpiebz.supabase.co';
-const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNhb2doZmVocXJqZ29iY3BpZWJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1NzgwMTcsImV4cCI6MjA4OTE1NDAxN30.3i6bHXbdmKppLt8r2n4YCD5rgeFnm6veeFPmReDmOMY';
