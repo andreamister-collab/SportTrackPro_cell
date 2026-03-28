c23490ab189a641f97e85795fe68c67694be3ee1
andreamister-collab <andrea.mister@gmail.com>
2026-03-28 02:36:55 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,3 +1,7 @@
-export const SUPABASE_URL = 'https://saoghfehqrjgobcpiebz.supabase.co';
-export const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNhb2doZmVocXJqZ29iY3BpZWJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1NzgwMTcsImV4cCI6MjA4OTE1NDAxN30.3i6bHXbdmKppLt8r2n4YCD5rgeFnm6veeFPmReDmOMY';
-export const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const SUPABASE_URL = 'https://saoghfehqrjgobcpiebz.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNhb2doZmVocXJqZ29iY3BpZWJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1NzgwMTcsImV4cCI6MjA4OTE1NDAxN30.3i6bHXbdmKppLt8r2n4YCD5rgeFnm6veeFPmReDmOMY';
+// Inizializziamo il client e lo rendiamo globale per l'app
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+// Rendiamo 'client' disponibile ovunque nel progetto
+window.client = client;
