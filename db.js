ee39e9bc3fc0c34ac893f7bf028f5b1729845347
Andrea Barba <andrea.mister@gmail.com>
2026-04-01 20:46:07 +0200

    Update Supabase URL and key in db.js

--- db.js
+++ db.js
@@ -1,3 +1,3 @@
-export const SUPABASE_URL = 'https://saoghfehqrjgobcpiebz.supabase.co';
-export const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNhb2doZmVocXJqZ29iY3BpZWJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1NzgwMTcsImV4cCI6MjA4OTE1NDAxN30.3i6bHXbdmKppLt8r2n4YCD5rgeFnm6veeFPmReDmOMY';
+export const SUPABASE_URL = 'https://jvcplfgozdqirzqfqwox.supabase.co';
+export const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2Y3BsZmdvemRxaXJ6cWZxd294Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1OTIxMjIsImV4cCI6MjA4OTE2ODEyMn0.-W724h8dTtaAyV1HxydYRddoD1oRxmEm0-zZc_8bxx8';
 export const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
